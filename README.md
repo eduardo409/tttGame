57e407a98057fdc80c0869db64163d5e07f5f7c0
eduardo409 <eduardo4cruz@yahoo.com>
2018-05-17 19:08:54 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+tic-tak-toe game that can be played on a network
---
+++ src/main/java/com/clouds/tttGame/TttGameApplication.java
@@ -0,0 +1,12 @@
+package com.clouds.tttGame;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TttGameApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TttGameApplication.class, args);
+	}
+}
